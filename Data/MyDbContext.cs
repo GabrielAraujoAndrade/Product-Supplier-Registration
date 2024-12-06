101378ad75d7c2dd9eec677dc04497dc451a95d8
Gabriel Andrade <117868071+GabrielAraujoAndrade@users.noreply.github.com>
2024-12-06 09:07:26 -0300

    Update MyDbContext.cs

--- Data/MyDbContext.cs
+++ Data/MyDbContext.cs
@@ -6,7 +6,7 @@ public class MyDbContext : DbContext
     public DbSet<Material> Materials { get; set; }
     public DbSet<Supplier> Suppliers { get; set; }
 
-    // Construtor que recebe DbContextOptions
+
     public MyDbContext(DbContextOptions<MyDbContext> options)
         : base(options)
     {
@@ -15,7 +15,7 @@ public class MyDbContext : DbContext
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<Material>()
-            .HasOne<Supplier>() // Sem incluir a propriedade de navegação
+            .HasOne<Supplier>() 
             .WithMany()
             .HasForeignKey(m => m.IdSupplier)
             .OnDelete(DeleteBehavior.Restrict);
